12146f37fd727b255724d84c8c2f6b3c15746d7b
Mariia <mashulkaegor@mail.ru>
2022-08-18 20:41:29 +0400

    add task06

---
+++ Task06/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int[] mas = new int[123];
+Random rnd = new Random();
+for(int i =0; i < mas.Length; i++)
+{
+mas[i]=rnd.Next();
+}
+int counter = 0;
+for(int i=0; i < mas.Length; i++)
+{
+if(mas[i]>=10 && mas[i] <= 99)
+{
+counter++;
+}
+}
+Console.WriteLine("Количество чисел из промежутка [10,99] = " + counter);
