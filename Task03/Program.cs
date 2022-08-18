cbd661e8dfc95eaa1960abd8ef123cc6ee191321
Mariia <mashulkaegor@mail.ru>
2022-08-18 20:47:36 +0400

    add Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3. Написать программу замену элементов массива на противоположные
+Console.WriteLine("Введите длину массива");
+int length = int.Parse(Console.ReadLine());
+int[] mas = new int[length];
+for(int i = 0; i < length; i++)
+{
+Console.WriteLine("Введите " + i +" элемент: ");
+mas[i]=int.Parse(Console.ReadLine());
+}
+for(int i = 0; i < mas.Length; i++)
+{
+mas[i]= mas[i]*(-1);
+}
+Console.WriteLine("Ответ: ");
+for(int i=0; i<mas.Length; i++)
+{
+Console.Write(mas[i] + " ");
+}
